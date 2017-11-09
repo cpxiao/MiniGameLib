e19eebd9578550fc0511ec62bab0e8551e7d0c9c
cpxiao <xchp2008@126.com>
2017-11-09 14:37:29 +0800

    first commit.

---
+++ app/src/main/java/com/cpxiao/AppConfig.java
@@ -0,0 +1,18 @@
+package com.cpxiao;
+
+/**
+ * @author cpxiao on 2017/3/1.
+ */
+public class AppConfig {
+    /**
+     * debug
+     */
+//    public static final boolean DEBUG = BuildConfig.DEBUG;
+    public static final boolean DEBUG = false;
+
+
+    public static final String TEST_DEVICE_FB = "3bcc341340550569d910c92a2dae2677";
+
+    public static final String TEST_DEVICE_ADMOB = "67F59060394DB36B95B18F5EE5B5D735";
+
+}
